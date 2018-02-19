bdc35b6261f050c7b46638e2de30aa5503ff0b26
J. Holmes <jholmes@n.io>
2018-02-19 10:53:06 -0700

    implementation

--- sliding_window_block.py
+++ sliding_window_block.py
@@ -1,12 +1,70 @@
+from collections import defaultdict
+from datetime import datetime
+
 from nio.block.base import Block
-from nio.properties import VersionProperty
+from nio.properties import IntProperty, VersionProperty, TimeDeltaProperty
+from nio.command import command
 
-
+@command("expire")
 class SlidingWindow(Block):
+    """Creates a sliding window of signals.
 
-    version = VersionProperty('0.1.0')
+    Examples:
+
+    { min_signals = 1, max_signals = 3 }
+    input:  ----1------2--------3--------4--------5-->
+    output: ----•------•--------•--------•--------•-->
+              [1]  [1,2]  [1,2,3]  [2,3,4]  [3,4,5]
+
+
+    { min_signals = 3, max_signals = 3 }
+    input:  ----1------2--------3--------4--------5-->
+    output: --------------------•--------•--------•-->
+                          [1,2,3]  [2,3,4]  [3,4,5]
+
+
+    { min_signals = 1, max_signals = 3, expiration: { millseconds: 500 } }
+    input:  ----1------2--------3--------4--| >500ms |---5-->
+    output: ----•------•--------•--------•--|        |---•-->
+              [1]  [1,2]  [1,2,3]  [2,3,4]             [5]
+    """
+
+    """TODO
+    - [x] Window Expriation
+    - [ ] Implement Group
+    - [ ] Implement Persistence
+    - [ ] Use Signal Expiration
+    """
+
+    version = VersionProperty("0.0.1")
+    min_signals = IntProperty(default=1, title='Min Signals')
+    max_signals = IntProperty(title='Max Signals')
+    expiration = TimeDeltaProperty(title='Window Expiration',
+                                   allow_none=True)
+
+    def __init__(self):
+        super().__init__()
+        self.buffer = []
+        self._last_recv = datetime.min
+
+    def expire(self):
+        self.logger.debug('Clearing the buffer window')
+        self.buffer.clear()
 
     def process_signals(self, signals):
+        now = datetime.utcnow()
+
+        hasExpiration = self.expiration() is not None
+        if hasExpiration and (self._last_recv + self.expiration()) < now:
+            self.logger.debug('The buffer window has expired')
+            self.buffer.clear()
+
+        self._last_recv = now
+
         for signal in signals:
-            pass
-        self.notify_signals(signals)
+            self.buffer.append(signal)
+
+        del self.buffer[:-self.max_signals()]
+
+        if len(self.buffer) >= self.min_signals():
+            self.notify_signals(self.buffer)
--- tests/test_sliding_window_block.py
+++ tests/test_sliding_window_block.py
@@ -1,19 +1,95 @@
+print(__name__)
+
+from time import sleep
+
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
+
 from ..sliding_window_block import SlidingWindow
 
-
 class TestSlidingWindow(NIOBlockTestCase):
+    def test_collect(self):
+        block = SlidingWindow()
+        self.configure_block(block, {
+            "min_signals": 1,
+            "max_signals": 4
+        })
+        block.start()
+        block.process_signals([Signal])
+        self.assert_num_signals_notified(1, block)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(3, block)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(6, block)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(10, block)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(14, block)
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
-        blk = SlidingWindow()
-        self.configure_block(blk, {})
-        blk.start()
-        blk.process_signals([Signal({"hello": "nio"})])
-        blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "nio"})
+
+    def test_min_signals(self):
+        block = SlidingWindow()
+        self.configure_block(block, {
+            "min_signals": 3,
+            "max_signals": 4
+        })
+        block.start()
+        block.process_signals([Signal()])
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(0, block)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(3, block)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(7, block)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(11, block)
+
+    def test_multi_signals(self):
+        block = SlidingWindow()
+        self.configure_block(block, {
+            "min_signals": 3,
+            "max_signals": 4
+        })
+        block.start()
+        first = Signal()
+        last = Signal()
+        block.process_signals([Signal(), first, Signal(), Signal(), last])
+        self.assert_num_signals_notified(4)
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][0] is first)
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][3] is last)
+
+    def test_window_slide(self):
+        block = SlidingWindow()
+        self.configure_block(block, {
+            "min_signals": 3,
+            "max_signals": 4
+        })
+        block.start()
+        first = Signal()
+        marker = Signal()
+        block.process_signals([Signal(), Signal(), Signal(), marker])
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][3] is marker)
+        block.process_signals([Signal()])
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][2] is marker)
+        block.process_signals([Signal()])
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][1] is marker)
+        block.process_signals([Signal()])
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][0] is marker)
+        block.process_signals([Signal()])
+        self.assertTrue(self.last_notified[DEFAULT_TERMINAL][0] is not marker)
+
+    def test_min_expiration(self):
+        block = SlidingWindow()
+        self.configure_block(block, {
+            "min_signals": 1,
+            "max_signals": 4,
+            "expiration": { "milliseconds": 200 }
+        })
+
+        block.start()
+        block.process_signals([Signal(), Signal(), Signal(), Signal()])
+        self.assert_num_signals_notified(4, block)
+        sleep(.3)
+        block.process_signals([Signal()])
+        self.assert_num_signals_notified(5, block)
